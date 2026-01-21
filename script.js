331e95686f3d74c91993bf8fe0871b57fdc60902
Devendragupta96 <devendragupta1302@gmail.com>
2026-01-21 15:04:44 +0530

    first code

---
+++ script.js
@@ -0,0 +1,92 @@
+console.log("Helllo students!!")
+
+// let obj={
+//     name:"Alex",
+//     age:45,
+//     Address:"GGN"
+// }
+// let obj2={
+//     name:"Alex",
+//     age:45,
+//     Address:"GGN"
+// }
+// obj.name="Bob"
+// console.log(obj)
+
+
+// const para=document.getElementById('para')
+// para.innerText="This is updated paragraph"
+// para.style.color="green"
+// console.log(para)
+
+
+// const para=document.querySelector('.para')
+// para.textContent="This is updated paragraph"
+// para.style.color="green"
+// console.log(para)
+
+// const para=document.getElementsByClassName('para')  //return HTMLCollection
+// para[0].innerText="This is updated paragraph"
+// para[1].style.color="green"
+// console.log(para)
+
+
+// const para2=document.querySelectorAll('.para') //return NodeList
+// para2[0].innerText="This is updated paragraph"
+// para2[1].style.color="green"
+// console.log(para2)
+
+// let arr=[23,45,235,56,45,3,2]
+// arr.forEach((ele)=>console.log(ele))
+
+
+// const container=document.querySelector(".container")
+// container.innerHTML="<h1>This is a heading</h1>"
+// console.log(container)
+
+// const btn=document.querySelector("button");
+// btn.classList.add("btn")
+// // btn.classList.remove('btn')
+// // btn.classList.toggle('btn')
+
+
+// function print(){
+//     console.log("Hello you have clicked the button")
+// }
+
+// btn.addEventListener('click',print)
+// btn.removeEventListener('click',print)
+// btn.addEventListener('click',()=>console.log("Hello you have clicked the button"))
+
+
+// const click=document.querySelector('#click')
+// // const stop=document.querySelector('#stop')
+
+// function message(event){
+//     console.log(event.key)
+//     console.log("Button Clicked!!")
+// }
+
+// click.addEventListener('mouseover',message)
+// click.addEventListener('keydown',message)
+// click.addEventListener('keyup',message)
+// stop.addEventListener('click',()=>{
+//     console.log("Stop button clicked!!")
+//     click.removeEventListener('click',message)
+// })
+
+// const form=document.querySelector('form')
+// form.addEventListener('submit',(event)=>{
+//     event.preventDefault()
+//     console.log("Form Submitted!!")
+// })
+
+// const div=document.querySelector('.container')
+// const div2=document.querySelector('.outer')
+// const btn=document.querySelector('button')
+
+
+// div.addEventListener('click',()=>{console.log("Clicked the Div")},false)
+// div2.addEventListener('click',()=>{console.log("Clicked the Outer Div")},false)
+// btn.addEventListener('click',()=>{console.log("Clicked the Button")},false)
+
