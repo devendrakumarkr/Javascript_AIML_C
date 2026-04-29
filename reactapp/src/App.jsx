37dfdc131d651b5cad845bd6520c7cf135291988
Devendragupta96 <devendragupta1302@gmail.com>
2026-04-29 15:31:54 +0530

    feat-pagination code

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -7,6 +7,8 @@ import Contact from './component/Contact'
 import { Route, Routes } from 'react-router-dom'
 import Profile from './component/Profile'
 import { UserContext } from './UserContext'
+import Search from './component/Search'
+import Product from './component/Product'
 
 function App() {
   const[data,setData]=useState("Sample data")
@@ -22,6 +24,8 @@ function App() {
           <Route path="/article" element={<Article/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="/profile/:id" element={<Profile/>}/>
+          <Route path='/search' element={<Search/>}/>
+          <Route path="/product" element={<Product/>}/>
         </Routes>
       </UserContext.Provider>
       
